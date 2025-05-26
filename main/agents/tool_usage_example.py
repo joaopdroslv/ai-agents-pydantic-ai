b1071b77a4a930ec5e31be5892d1f712c4d2638e
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-05-26 20:48:37 -0300

    adding explanatory comments

--- main/agents/tool_usage_example.py
+++ main/agents/tool_usage_example.py
@@ -11,13 +11,17 @@ agent_tools = Agent(
         "You are a price search assistant. "
         "You can use the 'get_product_price' tool to help. "
         "Always return in JSON format with 'product' and 'price'. "
-        "If the product's price is not available, "
-        "return in JSON format with 'product' and 'price', price should be an empty string."
+        "If the product's price is not available, price should be an empty string."
     ),
     tools=[Tool(function=get_product_price, takes_ctx=True)],
 )
 
-question = "What is the price of an coconut (the fruit)?"
+# question = "What is the price of an coconut (the fruit)?"
+# > It should return an empty price, as the product does not exist in the database
+# > {"product": "coconut", "price": ""}
+
+question = "I want to buy a orange, what is it price?"
+# > {"product": "orange", "price": "3.99"}
 
 answer = agent_tools.run_sync(question)
 
