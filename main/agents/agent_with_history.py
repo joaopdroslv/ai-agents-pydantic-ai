55cf9b189c71e1d940ef59530deb3e6d060c3ea0
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-06-04 11:28:51 -0300

    logfire integration added

--- main/agents/agent_with_history.py
+++ main/agents/agent_with_history.py
@@ -1,6 +1,17 @@
+import os
+from datetime import datetime
+
+import logfire
+from dotenv import load_dotenv
 from pydantic_ai import Agent
 
 from main.models.local_qwen import local_qwen
+
+load_dotenv()
+
+# logfire.configure(token=os.getenv("LOGFIRE_TOKEN", None))
+logfire.configure()  # Automatically get token from .env
+logfire.instrument_pydantic_ai()
 
 agent = Agent(local_qwen)
 
@@ -8,23 +19,21 @@ message_history = None
 
 
 def create_log(actor: str) -> str:
-    from datetime import datetime
-
     now = datetime.now()
-    return f"[CHAT] - [{now.date()} - {now.time().strftime('%H:%M:%S')}] - [{actor}]"
+    return f"[{now.date()} - {now.time().strftime('%H:%M:%S')}] [{actor}]"
 
 
 while True:
-    user_input = input(f"\n{create_log("You")}: ")
+    user_input = input(f"\n{create_log("You")}:\n\n ")
 
     # Allow to end the conversation
     if user_input.lower() in ["exit", "quit"]:
-        print(f"\n{create_log("System")}: Ending conversation with AI Agent.")
+        print(f"\n{create_log("System")}:\n\n Ending conversation with AI Agent.")
         break
 
     # If have a message history, send it
     result = agent.run_sync(user_input, message_history=message_history)
 
-    print(f"\n{create_log("AI Agent")}: ", result.output)
+    print(f"\n{create_log("AI Agent")}:\n\n ", result.output)
 
     message_history = result.all_messages()
